e011831ad6cf2262bfc5687bf64a2902931748da
Yurii Drutsul <druculurij@gmail.com>
2026-01-02 22:48:03 +0200

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -6,59 +6,41 @@ export interface Figure {
 export class Triangle implements Figure {
   shape: 'triangle' = 'triangle';
 
-  color: 'red' | 'green' | 'blue';
-
-  a: number;
-
-  b: number;
-
-  c: number;
-
   constructor(
-    color: 'red' | 'green' | 'blue',
-    a: number,
-    b: number,
-    c: number,
+    public color: 'red' | 'green' | 'blue',
+    public a: number,
+    public b: number,
+    public c: number,
   ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('All triangle sides must be positive numbers');
+    }
+
     const longestSide = Math.max(a, b, c);
     const sumOtherSides = a + b + c - longestSide;
 
     if (longestSide >= sumOtherSides) {
-      throw new Error('Incorrect values');
+      throw new Error('Incorrect triangle sides');
     }
-
-    this.color = color;
-    this.a = a;
-    this.b = b;
-    this.c = c;
   }
 
   getArea(): number {
-    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
-      throw new Error('All triangle sides must be positive numbers');
-    }
-
     const s = (this.a + this.b + this.c) / 2;
     const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
 
     return Math.floor(area * 100) / 100;
   }
 }
-
 export class Circle implements Figure {
   shape: 'circle' = 'circle';
 
-  color: 'red' | 'green' | 'blue';
-
-  radius: number;
-
-  constructor(color: 'red' | 'green' | 'blue', radius: number) {
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public radius: number,
+  ) {
     if (radius <= 0) {
-      throw new Error('Radius should be positive number');
+      throw new Error('Radius must be positive number');
     }
-
-    this.color = color;
-    this.radius = radius;
   }
 
   getArea(): number {
@@ -67,28 +49,23 @@ export class Circle implements Figure {
     return Math.floor(area * 100) / 100;
   }
 }
-
 export class Rectangle implements Figure {
   shape: 'rectangle' = 'rectangle';
 
-  color: 'red' | 'green' | 'blue';
-
-  width: number;
-
-  height: number;
-
-  constructor(color: 'red' | 'green' | 'blue', width: number, height: number) {
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public width: number,
+    public height: number,
+  ) {
     if (width <= 0 || height <= 0) {
-      throw new Error('Values should be positive');
+      throw new Error('Width and height must be positive numbers');
     }
-
-    this.color = color;
-    this.width = width;
-    this.height = height;
   }
 
   getArea(): number {
-    return Math.floor(this.width * this.height * 100) / 100;
+    const area = this.width * this.height;
+
+    return Math.floor(area * 100) / 100;
   }
 }
 
